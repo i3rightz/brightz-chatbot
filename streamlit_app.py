5d08865f4758c7cbae5aaf5ba711d585f7cf5940
i3rightz <34789386+i3rightz@users.noreply.github.com>
2024-10-12 03:52:06 +0000

    google.generativeai

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,7 @@ import google.generativeai as genai
 st.title("🐧 My chatbot app")
 st.subheader("Conversation")
 # Capture Gemini API Key
-gemini_api_key = st.text_input("Gemini API Key: ", placeholder="Type your API Key
-here...", type="password")
+gemini_api_key = st.text_input("Gemini API Key: ", placeholder="Type your API Key here...", type="password")
 # Initialize the Gemini Model
 if gemini_api_key:
     try:
